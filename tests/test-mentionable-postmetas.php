b6c9ae48f229ee6920badf5c6ddb9ca064d16a9a
Topher DeRosia <topher@x-team.com>
2013-10-30 16:26:08 -0400

    issue-6: set up tests for actions in constructor

--- tests/test-mentionable-postmetas.php
+++ tests/test-mentionable-postmetas.php
@@ -32,7 +32,8 @@ class Test_Mentionable_Postmetas extends WP_UnitTestCase {
 	}
 
 	public function test_constructor() {
-		//@todo
+		$this->assertEquals( 10, has_action( 'save_post', array( $this->plugin->mentionable_postmetas, 'update_mention_meta' ) ), 'update_mention_meta action is not defined or has the wrong priority' );
+		$this->assertEquals( 10, has_action( 'pre_post_update', array( $this->plugin->mentionable_postmetas, 'remove_post_meta' ) ), 'remove_post_meta action is not defined or has the wrong priority' );
 	}
 
 }
